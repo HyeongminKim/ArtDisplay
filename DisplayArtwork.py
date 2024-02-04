d85f5576135e9dc14e95bf935d0751b8512a903f
HyeongminKim <assa0620@gmail.com>
2024-02-04 13:47:49 +0900

    Added image refresh feature

--- DisplayArtwork.py
+++ DisplayArtwork.py
@@ -1,3 +1,6 @@
+wallpaper_window = None
+label = None
+
 try:
     from os import environ
     from tkinter import Tk, Label
@@ -9,36 +12,49 @@ except (ImportError, ModuleNotFoundError) as error:
     exit(1)
 
 # usage: `export artwork_res=0` or write .bash_profile to configure artwork size
+updateInterval = environ.get('artwork_inv')
 fullscreen = environ.get('artwork_fs')
 resolutionX = environ.get('artwork_resX')
 resolutionY = environ.get('artwork_resY')
-if (resolutionX is None or resolutionY is None):
+
+if (resolutionX is None or resolutionY is None or updateInterval is None):
     print("resolution vector does not configured yet. Please set resolution vector first!")
+    print("Splash image reload interval was not configured!")
+    print("supported environ values:")
+    print("\tartwork_inv\tupdate splash image interval")
+    print("\tartwork_fs\tmake fullscreen display artwork")
+    print("\tartwork_resX\tsplash image X")
+    print("\tartwork_resY\tsplash image Y")
     exit(1)
 
-backgroundSource = f"https://unsplash.it/{resolutionX}/{resolutionY}/?random"
-print(f"Display artwork resolution vector: {resolutionX}x{resolutionY}")
-print(f"Display artwork target: {backgroundSource}")
+def downloadSplashSource():
+    backgroundSource = f"https://unsplash.it/{resolutionX}/{resolutionY}/?random"
+    print(f"Display artwork resolution vector: {resolutionX}x{resolutionY}")
+    print(f"Display artwork target: {backgroundSource}")
+    try:
+        with urllib.request.urlopen(backgroundSource) as url_parse:
+            return url_parse.read()
+    except (HTTPError) as error:
+        print("error: Background source could not download at this time.")
+        print(f"respond status code: {error.code}")
+        return None
 
-try:
-    with urllib.request.urlopen(backgroundSource) as url_parse:
-        raw_data = url_parse.read()
-except (HTTPError) as error:
-    print("error: Background source could not download at this time.")
-    print(f"respond status code: {error.code}")
-    exit(1)
+def refreshSplashImage():
+    newImage = ImageTk.PhotoImage(Image.open(io.BytesIO(downloadSplashSource())))
+    label['image'] = newImage
+    label.image = newImage
+    wallpaper_window.after(updateInterval, refreshSplashImage)
 
 wallpaper_window = Tk()
 wallpaper_window.geometry("1024x768")
 wallpaper_window.title("")
 
-imageViewer = ImageTk.PhotoImage(Image.open(io.BytesIO(raw_data)))
-label = Label(wallpaper_window, image=imageViewer)
-label.image = imageViewer
+label = Label(wallpaper_window, text="")
 label.place(relx=0.5, rely=0.5, anchor='center')
 
 if (fullscreen is not None and fullscreen == "true"):
     wallpaper_window.attributes('-fullscreen', True)
 
+refreshSplashImage()
 wallpaper_window.mainloop()
 
